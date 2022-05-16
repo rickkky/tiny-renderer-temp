71588ac4a678b2a20007b3fa052c5e8b72046d5f
rickkky <rickkky@foxmail.com>
2022-05-16 13:11:34 +0800

    exmaple: texture demo

--- demo/main.js
+++ demo/main.js
@@ -2,6 +2,7 @@ import {
     TinyRenderer,
     loadObjModel,
     renderMesh,
+    TgaImage,
 } from '../dist/tiny-renderer.js';
 
 const canvas = document.querySelector('canvas');
@@ -9,8 +10,10 @@ const ctx = canvas.getContext('2d');
 
 const renderer = new TinyRenderer(canvas.width, canvas.height);
 
-const mesh = await loadObjModel('./model.obj');
-renderMesh(renderer, mesh);
+const mesh = await loadObjModel('./model/head.obj');
+const texture = await TgaImage.open('./model/head-diffuse.tga');
+
+renderMesh(renderer, mesh, texture.getImageData());
 
 // renderer.triangle([400.3, 400.9], [100, 150], [300, 200], [255, 0, 0, 255]);
 // renderer.triangle([400.3, 400.9], [100, 150], [200, 300], [0, 255, 0, 255]);
