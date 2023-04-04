3cf7ec3737fe3355ea05c954dbd15f5f4c1a4b52
ricky <rickkky@foxmail.com>
2023-04-04 18:11:05 +0800

    refactor: update vector.ts

--- src/vector.ts
+++ src/vector.ts
@@ -1,45 +1,28 @@
-export class Vector extends Array<number> {
-    static create(size: number) {
-        return new Vector(...new Array(size).fill(0));
-    }
-
-    static add(a: Vector, b: Vector) {
-        return a.clone().add(b);
-    }
-
-    static substract(a: Vector, b: Vector) {
-        return a.clone().substract(b);
-    }
-
-    static scale(v: Vector, n: number) {
-        return v.clone().scale(n);
-    }
-
-    static normalize(v: Vector) {
-        return v.clone().normalize();
-    }
-
-    static dot(a: Vector, b: Vector) {
-        return a.dot(b);
-    }
-
-    static cross(a: Vector, b: Vector) {
-        return a.clone().cross(b);
-    }
+export abstract class Vector {
+    protected array: number[] = [];
 
     constructor(...args: number[]) {
         if (args.length < 2) {
             throw new Error('Vector must have at least 2 dimensions');
         }
-        super(...args);
+        this.array[0] = args[0] || 0;
+        this.array[1] = args[1] || 0;
     }
 
     get dimension() {
-        return this.length;
+        return this.array.length;
     }
 
     get size() {
-        return Math.sqrt(this.reduce((acc, n) => acc + n * n, 0));
+        return Math.sqrt(this.array.reduce((acc, n) => acc + n * n, 0));
+    }
+
+    get 0() {
+        return this.array[0];
+    }
+
+    set 0(n: number) {
+        this.array[0] = n;
     }
 
     get x() {
@@ -50,6 +33,14 @@ export class Vector extends Array<number> {
         this[0] = n;
     }
 
+    get 1() {
+        return this.array[1];
+    }
+
+    set 1(n: number) {
+        this.array[1] = n;
+    }
+
     get y() {
         return this[1];
     }
@@ -58,43 +49,29 @@ export class Vector extends Array<number> {
         this[1] = n;
     }
 
-    get z() {
-        return this[2];
+    [Symbol.iterator]() {
+        return this.array[Symbol.iterator]();
     }
 
-    set z(n: number) {
-        this[2] = n;
-    }
+    abstract clone(): Vector;
 
-    get w() {
-        return this[3];
-    }
-
-    set w(n: number) {
-        this[3] = n;
-    }
-
-    clone() {
-        return new Vector(...this);
-    }
-
-    add(v: Vector) {
-        for (const i of this.keys()) {
-            this[i] += v[i];
+    add(v: typeof this) {
+        for (const i of this.array.keys()) {
+            this.array[i] += v.array[i];
         }
         return this;
     }
 
-    substract(v: Vector) {
-        for (const i of this.keys()) {
-            this[i] -= v[i];
+    substract(v: typeof this) {
+        for (const i of this.array.keys()) {
+            this.array[i] -= v.array[i];
         }
         return this;
     }
 
     scale(n: number) {
-        for (const i of this.keys()) {
-            this[i] *= n;
+        for (const i of this.array.keys()) {
+            this.array[i] *= n;
         }
         return this;
     }
@@ -107,19 +84,80 @@ export class Vector extends Array<number> {
         return this.scale(1 / size);
     }
 
-    dot(v: Vector) {
-        return this.reduce((acc, n, i) => acc + n * v[i], 0);
+    dot(v: typeof this) {
+        return this.array.reduce((acc, n, i) => acc + n * v.array[i], 0);
+    }
+}
+
+export class Vector2 extends Vector {
+    clone() {
+        return new Vector2(this.x, this.y);
+    }
+}
+
+export class Vector3 extends Vector {
+    constructor(...args: number[]) {
+        super(...args);
+        this.array[2] = args[2] || 0;
     }
 
-    cross(v: Vector) {
-        if (this.dimension !== 3 || v.dimension !== 3) {
-            throw new Error('Cross product is only defined for 3D vectors');
-        }
+    protected Constructor() {
+        return Vector3;
+    }
+
+    get 2() {
+        return this.array[2];
+    }
+
+    set 2(n: number) {
+        this.array[2] = n;
+    }
+
+    get z() {
+        return this[2];
+    }
+
+    set z(n: number) {
+        this[2] = n;
+    }
+
+    clone() {
+        return new Vector3(this.x, this.y, this.z);
+    }
+
+    cross(v: Vector3) {
         const [x0, y0, z0] = this;
         const [x1, y1, z1] = v;
         this[0] = y0 * z1 - z0 * y1;
         this[1] = z0 * x1 - x0 * z1;
         this[2] = x0 * y1 - y0 * x1;
         return this;
+    }
+}
+
+export class Vector4 extends Vector3 {
+    constructor(...args: number[]) {
+        super(...args);
+        this.array[3] = args[3] || 0;
+    }
+
+    get 3() {
+        return this.array[3];
+    }
+
+    set 3(n: number) {
+        this.array[3] = n;
+    }
+
+    get w() {
+        return this[3];
+    }
+
+    set w(n: number) {
+        this[3] = n;
+    }
+
+    clone() {
+        return new Vector4(this.x, this.y, this.z, this.w);
     }
 }
